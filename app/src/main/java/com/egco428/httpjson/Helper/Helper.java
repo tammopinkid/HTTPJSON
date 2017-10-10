39b95b6823d2572acf771b4df02e6f2448113289
tammopinkid <tammo.pinkid@gmail.com>
2017-10-10 11:55:51 +0700

    First Commit

---
+++ app/src/main/java/com/egco428/httpjson/Helper/Helper.java
@@ -0,0 +1,8 @@
+package com.egco428.httpjson.Helper;
+
+/**
+ * Created by 6272user on 10/10/2017 AD.
+ */
+
+public class Helper {
+}
---
+++ app/src/main/java/com/egco428/httpjson/Models/Quote.kt
@@ -0,0 +1,7 @@
+package com.egco428.httpjson.Models
+
+/**
+ * Created by 6272user on 10/10/2017 AD.
+ */
+class Quote {
+}
